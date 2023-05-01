aeb6000b279c5e75fe8b8737cd4907ead760de65
ThatHomr <34852381a@gmail.com>
2023-05-01 19:19:29 +0900

    23-05-01 박성준 reference update

--- referenceapp/urls.py
+++ referenceapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.reference),
-
+    ########### [지도 시각화] 처리 ########
+    ### http://127.0.0.1:8000/nonmodel/map_view/
+    path('map_view/', views.map_Visualization),
 
 ]
